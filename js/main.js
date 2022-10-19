cd07b88a320be6a8ed83e80345f72b35ccbae6e3
shalabyelectronics <mhdshalabystar@gmail.com>
2022-10-19 07:50:07 +0300

    Play the letter sound when the key pressed.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 document.querySelector("body").addEventListener("keypress",(event)=>{
     if (alphabet.includes(event.key)){
-        console.log(`The ${event.key} is available`)
+        //console.log(`The ${event.key} is available`)
+        let letter = `./sounds/${event.key}.wav`;
+        let letter_sound = new Audio(letter);
+        letter_sound.play();
+        document.querySelector(".default-mode").innerText = event.key.toUpperCase();
+        document.querySelector(".default-mode").classList.add("english-letter");
     }else{
-        console.log(`The ${event.key} is not available`)
+        //console.log(`The ${event.key} is not available`)
     }
 })
