bc0d8f9e8096089dd60c0efc3ed989a80299d595
shalabyelectronics <mhdshalabystar@gmail.com>
2022-10-19 07:57:20 +0300

    change the event listener type to keyup instaed of keypress

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-document.querySelector("body").addEventListener("keypress",(event)=>{
+document.querySelector("body").addEventListener("keyup",(event)=>{
     if (alphabet.includes(event.key)){
         //console.log(`The ${event.key} is available`)
         let letter = `./sounds/${event.key}.wav`;
@@ -9,5 +9,8 @@ document.querySelector("body").addEventListener("keypress",(event)=>{
         document.querySelector(".default-mode").classList.add("english-letter");
     }else{
         //console.log(`The ${event.key} is not available`)
+        let message = "أضغط على أي حرف انجليزي لتستمع الى نطقه"
+        document.querySelector(".default-mode").innerText = message;
+        document.querySelector(".default-mode").classList.remove("english-letter");
     }
 })
