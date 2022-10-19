d6fb1ba803f2dfc8e929b180f5fe42c4a4b3fb89
Mohamed Shalaby <mhdshalabystar@gmail.com>
2022-10-19 14:49:33 +0300

    Add a textarea to get it work on smart phone

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 document.querySelector("body").addEventListener("keyup",(event)=>{
-    if (alphabet.includes(event.key)){
+    playLetterSound(event.key);
+})
+
+document.querySelector("#myInput").addEventListener("input",(event)=>{
+    let getText = document.querySelector("#myInput");
+    playLetterSound(getText.value)
+    getText.value = "";
+})
+
+function playLetterSound(char){
+    if (alphabet.includes(char)){
         //console.log(`The ${event.key} is available`)
-        let letter = `./sounds/${event.key}.wav`;
+        let letter = `./sounds/${char}.wav`;
         let letter_sound = new Audio(letter);
         letter_sound.play();
-        document.querySelector(".default-mode").innerText = event.key.toUpperCase();
+        document.querySelector(".default-mode").innerText = char.toUpperCase();
         document.querySelector(".default-mode").classList.add("english-letter");
     }else{
         //console.log(`The ${event.key} is not available`)
@@ -13,4 +23,4 @@ document.querySelector("body").addEventListener("keyup",(event)=>{
         document.querySelector(".default-mode").innerText = message;
         document.querySelector(".default-mode").classList.remove("english-letter");
     }
-})
+}
