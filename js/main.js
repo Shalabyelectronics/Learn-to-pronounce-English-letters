3627d399edec63a9a92966573e053a0025e1f54b
shalabyelectronics <mhdshalabystar@gmail.com>
2022-10-21 14:31:35 +0300

    convert VanillaJS to jQuery

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
-const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const alphabet = /^[a-zA-Z]{1}$/
 
 let details = navigator.userAgent;
 let regexp = /android|iphone/i;
 let isMobileDevice = regexp.test(details);
 if (isMobileDevice){
-    document.querySelector("textarea").style.setProperty("display","block");
-    document.querySelector("#myInput").addEventListener("input",()=>{
-        let getText = document.querySelector("#myInput");
-        playLetterSound(getText.value)
-        getText.value = "";
+    $("input").css("display","block");
+    document.querySelector("#myInput").addEventListener("input",(event)=>{   
+        playLetterSound(event.data)
+        $("#myInput").val("");
     })
 }else{
-    document.querySelector("body").addEventListener("keyup",(event)=>{
+    $("body").keyup((event)=>{
         playLetterSound(event.key);
     })
 }
@@ -20,17 +19,17 @@ if (isMobileDevice){
 
 
 function playLetterSound(char){
-    if (alphabet.includes(char)){
+    if (alphabet.test(char)){
         //console.log(`The ${event.key} is available`)
-        let letter = `./sounds/${char}.wav`;
+        let letter = `./sounds/${char.toLowerCase()}.wav`;
         let letter_sound = new Audio(letter);
         letter_sound.play();
-        document.querySelector(".default-mode").innerText = char.toUpperCase();
-        document.querySelector(".default-mode").classList.add("english-letter");
+        $(".default-mode").text(char.toUpperCase());
+        $(".default-mode").addClass("english-letter");
     }else{
         //console.log(`The ${event.key} is not available`)
         let message = "أضغط على أي حرف انجليزي لتستمع الى نطقه"
-        document.querySelector(".default-mode").innerText = message;
-        document.querySelector(".default-mode").classList.remove("english-letter");
+        $(".default-mode").text(message);
+        $(".default-mode").removeClass("english-letter");
     }
 }
